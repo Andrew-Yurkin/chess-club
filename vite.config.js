1a52d5f95d6d32a939b11f779dd70aab1766a229
Andrew-Yurkin <andrew.yurkin.v@gmail.com>
2026-05-12 22:50:29 +0400

    fix performance

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,31 @@ import viteImagemin from 'vite-plugin-imagemin'
 export default defineConfig({
   plugins: [
     viteImagemin({
-      mozjpeg: { quality: 75 },
-      pngquant: { quality: [0.6, 0.8] },
-      webp: { quality: 75 },
+      mozjpeg: {
+        quality: 65,
+        progressive: true,
+      },
+
+      pngquant: {
+        quality: [0.5, 0.7],
+        speed: 1,
+      },
+
+      svgo: {
+        plugins: [
+          { name: 'removeViewBox', active: false },
+          { name: 'removeEmptyAttrs', active: false },
+        ],
+      },
+
+      webp: {
+        quality: 70,
+      },
+
+      avif: {
+        quality: 50,
+      },
     }),
   ],
+  base: '/chess-club/',
 })
